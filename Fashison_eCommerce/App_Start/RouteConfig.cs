51939ec32472fd62ad2672d4f7c85d549a5315e3
THIENNHI-LAPTOP\LENOVO <nhilx150017@gmail.com>
2020-11-13 18:16:30 +0700

    Add Areas Admin,  Template Layout Dashboard Admin

--- Fashison_eCommerce/App_Start/RouteConfig.cs
+++ Fashison_eCommerce/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Fashison_eCommerce
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "User", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Fashison_eCommerce.Controllers" }
             );
 
         }
