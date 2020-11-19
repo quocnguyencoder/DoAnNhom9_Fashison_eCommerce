45165b0411ff91e17235400ef001fc4296231d63
Nguyen Duc Hoa <vic.nguyenduchoa@gmail.com>
2020-11-19 14:37:49 +0700

    update controller prodOfStore

--- Fashison_eCommerce/Models/User.cs
+++ Fashison_eCommerce/Models/User.cs
@@ -18,6 +18,7 @@ namespace Fashison_eCommerce.Models
         public User()
         {
             this.Stores = new HashSet<Store>();
+            this.Carts = new HashSet<Cart>();
         }
     
         public int Id { get; set; }
@@ -34,5 +35,7 @@ namespace Fashison_eCommerce.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Store> Stores { get; set; }
         public virtual Role Role { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cart> Carts { get; set; }
     }
 }
