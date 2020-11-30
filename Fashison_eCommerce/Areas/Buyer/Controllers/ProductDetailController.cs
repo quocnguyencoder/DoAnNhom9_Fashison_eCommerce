113f86aa7aa70e886d88936824e0d54ce9e083c1
Nguyen Duc Hoa <vic.nguyenduchoa@gmail.com>
2020-11-30 09:08:39 +0700

    update

--- Fashison_eCommerce/Areas/Buyer/Controllers/ProductDetailController.cs
+++ Fashison_eCommerce/Areas/Buyer/Controllers/ProductDetailController.cs
@@ -11,28 +11,15 @@ namespace Fashison_eCommerce.Areas.Buyer.Controllers
 {
     public class ProductDetailController : Controller
     {
-        public int idProd;
         // GET: Buyer/ProductDetail
         public ActionResult Index()
         {
-            if (Session["ID"] != null)
-            {
-                int id = Convert.ToInt32(Session["ID"]);
-                ProductClient CC = new ProductClient();
-                ViewBag.listProducts = CC.find(id);
-
-            }
-            else
-            {
-                ProductClient CC = new ProductClient();
-                ViewBag.listProducts = CC.find(4);
-            }
-
             return View();
         }
 
         DB_A6A231_DAQLTMDTEntities db = new DB_A6A231_DAQLTMDTEntities();
 
+        // menu loai san pham
         [ChildActionOnly]
         public PartialViewResult ProductType()
         {
@@ -43,16 +30,21 @@ namespace Fashison_eCommerce.Areas.Buyer.Controllers
 
         public ActionResult Details(int id)
         {
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            //}
-            idProd = id;
+            if (id == default)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+
+            // chi tiet san pham
             ProductClient CC = new ProductClient();
             ViewBag.detail = CC.find(id);
+
+            // thong tin nhan hieu san pham
             int brandID = Convert.ToInt32(CC.find(id).BrandID);
             ViewBag.brand = db.Brands.Where(x => x.BrandID == brandID).FirstOrDefault();
 
+            // Lay thong tin cua hang
             int storeID = Convert.ToInt32(ViewBag.detail.Store_ID);
             Getstore(storeID);
 
@@ -65,12 +57,14 @@ namespace Fashison_eCommerce.Areas.Buyer.Controllers
             return View();
         }
 
+        // Lay thong tin cua hang
         public ActionResult Getstore(int id)
         {
             ViewBag.store = db.Stores.Where(x => x.Store_ID == id).FirstOrDefault();
             return View();
         }
 
+        // Lay thong tin loại con
         public ActionResult GetSubtype()
         {
             ViewBag.subType = db.Product_Type.ToList();
