0db477b4ad935850a70274786ee639de28cdf594
Nguyen Duc Hoa <vic.nguyenduchoa@gmail.com>
2020-12-15 08:39:34 +0700

    search

--- Fashison_eCommerce/Areas/Buyer/Controllers/SearchController.cs
+++ Fashison_eCommerce/Areas/Buyer/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fashison_eCommerce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,20 @@ namespace Fashison_eCommerce.Areas.Buyer.Controllers
     {
         // GET: Buyer/Search
         public ActionResult Index()
+        
         {
+            string name = Request.Form["name"];
+            int id = Convert.ToInt32(Request.Form["mainType"]);
+
+            Search(name, id);
+            return View();
+        }
+
+        public ActionResult Search(string name, int typeid)
+        {
+            ProductClient PC = new ProductClient();
+            ViewBag.Product = PC.search(typeid, name);
+            ViewBag.Count = PC.search(typeid, name).Count();
             return View();
         }
     }
