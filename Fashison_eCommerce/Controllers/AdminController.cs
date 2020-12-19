a7775bd40d4559e05800018fb22b3e216d20b723
Nguyen Duc Hoa <vic.nguyenduchoa@gmail.com>
2020-12-19 18:26:25 +0700

    add shop

---
+++ Fashison_eCommerce/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fashison_eCommerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+
+        public ActionResult Index()
+        {
+            if (Session["UserID"] == null)
+            {
+                return View("Login");
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+
+
+    }
+}
